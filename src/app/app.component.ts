16abc06d910e325cd0c8f8d73a3827483ac5e5a9
Suraj <suraj.bande98@gmail.com>
2022-08-05 18:24:35 +0530

    test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,10 @@
 import { Component } from '@angular/core';
 
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  // title = 'bank-app';
-  // bankTypes = BankMap;
-  // getInterest(bankType: string) {
-  //   const bankAccount: BankAccount = EntityMap.get(bankType)!;
-  //   if (bankAccount) {
-  //     const interest = bankAccount.calculateInterest();
-  //     console.log(interest);
-  //   }
-  // }
+  title = 'app';
 }
